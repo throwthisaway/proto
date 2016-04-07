cdbcb7d18a4a4276b335d5b3c7c6aebbddb2fd83
throwthisaway <throwthisaway@freemail.hu>
2016-04-07 22:29:45 +0200

    ninja build support

--- emc_ogl/build.bat
+++ emc_ogl/build.bat
@@ -1,9 +1,17 @@
 set MeshLoaderPath=..\..\MeshLoader\
+set MeshExportPath=..\..\MeshExport\
 emcc main.cpp^
  %MeshLoaderPath%MeshLoader.cpp^
  %MeshLoaderPath%types.cpp^
- %MeshLoaderPath%MeshSurface.cpp -v -s USE_GLFW=3 -s NO_EXIT_RUNTIME=1^
+ %MeshExportPath%MeshCommon\FileReader.cpp^
+ %MeshExportPath%MeshCommon\File.cpp^
+ %MeshExportPath%MeshView2005\Img\Tga.cpp^
+ %MeshLoaderPath%MeshSurface.cpp^
+ -v -s USE_GLFW=3 -s NO_EXIT_RUNTIME=1^
  -I..\..\..\3rdparty\glm^
  -I..\..\..\3rdparty\GSL\include^
- -i%MeshLoaderPath% -DGLM -std=c++14 --embed-file asset^
+ -i%MeshLoaderPath%^
+ -i%MeshExportPath%MeshCommon^
+ -i%MeshExportPath%MeshView2005\Img^
+ -DGLM -std=c++14 --embed-file asset^
  -o main.js

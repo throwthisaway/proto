2eb16ac4d87949fa1c3d7a5458031941d127978e
throwthisaway <throwthisaway@freemail.hu>
2016-04-21 22:40:22 +0200

    RT

--- emc_ogl/main.cpp
+++ emc_ogl/main.cpp
@@ -31,7 +31,8 @@
 #include "Shader/Color.h"
 #include "Shader/ColorPosAttrib.h"
 #include "Shader/Texture.h"
-#include "Shader/RTShader.h"
+#include "RT.h"
+
 //#define VAO_SUPPORT
 #define DEBUG_REL
 #define CLIENTID_LEN 5
@@ -51,6 +52,7 @@ inline glm::vec3 RotateZ(const glm::vec3& v, const glm::vec3& c, float r) {
 	//return{std::cos(r) * (v.x - c.x), std::sin(r) * (v.y - c.y), 0.f };
 	return glm::rotateZ(v - c, r);
 }
+
 static const glm::vec4 c64[] = {/* {0.f/255.f,0.f/255.f,0.f/255.f, 1.f},*/{ 255.f/255.f, 255.f/255.f, 255.f/255.f, 1.f},{ 152.f/255.f, 75.f/255.f, 67.f/255.f, 1.f},{ 121.f/255.f, 193.f/255.f, 200.f/255.f, 1.f},{ 155.f/255.f, 81.f/255.f, 165.f/255.f, 1.f},{ 104.f/255.f, 174.f/255.f, 92.f/255.f, 1.f},{ 82.f/255.f, 66.f/255.f, 157.f/255.f, 1.f},{ 201.f/255.f, 214.f/255.f, 132.f/255.f, 1.f},
 { 155.f/255.f, 103.f/255.f, 57.f/255.f, 1.f},{ 106.f/255.f, 84.f/255.f, 0.f/255.f, 1.f},{ 195.f/255.f, 123.f/255.f, 117.f/255.f, 1.f},{ 99.f/255.f, 99.f/255.f, 99.f/255.f, 1.f},{ 138.f/255.f, 138.f/255.f, 138.f/255.f, 1.f},{ 163.f/255.f, 229.f/255.f, 153.f/255.f, 1.f},{ 138.f/255.f, 123.f/255.f, 206.f/255.f, 1.f},{ 173.f/255.f, 173.f/255.f, 173.f/255.f, 1.f } },
 cpc[] = {/* { 0.f/255.f,0.f/255.f,0.f/255.f, 1.f},*/{ 0.f/255.f, 0.f/255.f, 128.f/255.f, 1.f},{ 0.f/255.f, 0.f/255.f, 255.f/255.f, 1.f},{ 128.f/255.f, 0.f/255.f, 0.f/255.f, 1.f},{ 128.f/255.f, 0.f/255.f, 128.f/255.f, 1.f},{ 128.f/255.f, 0.f/255.f, 255.f/255.f, 1.f},{ 255.f/255.f, 0.f/255.f, 0.f/255.f, 1.f},{ 255.f/255.f, 0.f/255.f, 128.f/255.f, 1.f},{ 255.f/255.f, 0.f/255.f, 255.f/255.f, 1.f},
@@ -76,7 +78,7 @@ static struct {
 
 	std::string host = "localhost";
 	unsigned short port = 8000;
-	int width = 640, height = 480;
+	unsigned int width = 640, height = 480;
 	std::string sessionID;
 	std::unique_ptr<Scene> scene;
 	std::unique_ptr<Client> ws;
@@ -358,9 +360,11 @@ namespace Asset {
 //				}
 //				masks_end = images.size();
 				images.push_back(LoadImage(PATH_PREFIX"asset//masks//mask_straight_3x4.tga"));
-				masks_image_index = 0;
 				images.push_back(LoadImage(PATH_PREFIX"asset//masks//mask_staggered_6x8.tga"));
-
+				images.push_back(LoadImage(PATH_PREFIX"asset//masks//mask_straight_3x3_scanline.tga"));
+				images.push_back(LoadImage(PATH_PREFIX"asset//masks//mask_straight_3x4_scanline.tga"));
+				images.push_back(LoadImage(PATH_PREFIX"asset//masks//mask_straight_3x4_scanline2.tga"));
+				masks_image_index = 4;
 #ifdef __EMSCRIPTEN__
 				emscripten_log(EM_LOG_CONSOLE, "image count %d\n", images.size());
 #endif
@@ -530,168 +534,6 @@ struct Camera {
 			pos.y -= d.y - top;
 		view = glm::translate({}, pos);
 		vp = proj * view;
-	}
-};
-class RT {
-	Shader::RTShader shader;
-	GLuint vao, vbo1, vbo2, txt, rbo, fbo, mask_uv;
-	const size_t w = 512/*globals.width*/, h = 512/*globals.height*/;
-public:
-	GLuint mask = 0;
-	float maskOpacity = 1.f, maskRepeat = .75f;
-	RT() {
-		static const GLfloat g_vertex_buffer_data[] = { -1.0f, -1.0f, 0.0f,
-			1.f, -1.0f, 0.0f,
-			-1.0f,  1.0f, 0.0f, 
-			1.f, 1.f, 0.0f};
-		glGenBuffers(1, &vbo1);
-		glBindBuffer(GL_ARRAY_BUFFER, vbo1);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
-		static const GLfloat g_uv_buffer_data[] = { 0.f, 0.f,
-			1.f,.0f,
-			0.f,  1.0f,
-			1.f, 1.f};
-		glGenBuffers(1, &vbo2);
-		glBindBuffer(GL_ARRAY_BUFFER, vbo2);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_buffer_data), g_uv_buffer_data, GL_STATIC_DRAW);
-#ifdef VAO_SUPPORT
-		glGenVertexArrays(1, &vao);
-		glBindVertexArray(vao);
-		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, vbo1);
-		glVertexAttribPointer(0,
-			3,
-			GL_FLOAT,
-			GL_FALSE,
-			0,
-			(void*)0);
-		glEnableVertexAttribArray(1);
-		glBindBuffer(GL_ARRAY_BUFFER, vbo2);
-		glVertexAttribPointer(1,
-			2,
-			GL_FLOAT,
-			GL_FALSE,
-			0,
-			(void*)0);
-		glBindVertexArray(0);
-#endif
-		glGenTextures(1, &txt);
-		glBindTexture(GL_TEXTURE_2D, txt);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-		//glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_FALSE);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0,
-			GL_RGBA, GL_UNSIGNED_BYTE, 0);
-		glBindTexture(GL_TEXTURE_2D, 0);
-
-		// Depth texture...
-		glGenRenderbuffers(1, &rbo);
-		glBindRenderbuffer(GL_RENDERBUFFER, rbo);
-		glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_STENCIL, w, h);
-		glBindRenderbuffer(GL_RENDERBUFFER, 0);
-		//...Depth texture
-
-		glGenFramebuffers(1, &fbo);
-		glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
-		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, txt, 0);
-		glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-		glGenBuffers(1, &mask_uv);
-	}
-	
-	void GenMaskUVBufferData(float sw, float sh, float iw, float ih) {
-		//const GLfloat uv_buffer_data[] = { 0.f, 0.f,
-		//	1.f,.0f,
-		//	0.f,  1.0f,
-		//	1.f, 1.f }; 
-		/*const GLfloat uv_buffer_data[] = { 0.f, 0.f,
-			sw / iw,.0f,
-			0.f,  sh / ih,
-			sw / iw, sh / ih };*/
-		const auto w = sw / iw, h = sh / ih;
-		const GLfloat uv_buffer_data[] = { 0.f, 0.f,
-			w, .0f,
-			0.f, h,
-			w, h };
-		glBindBuffer(GL_ARRAY_BUFFER, mask_uv);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(uv_buffer_data), uv_buffer_data, GL_STATIC_DRAW);
-	}
-	void Set() {
-		glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-		glViewport(0, 0, w, h);
-	}
-	void Reset() {
-		glBindFramebuffer(GL_FRAMEBUFFER, 0);
-		glViewport(0, 0, globals.width, globals.height);
-		glDisable(GL_DEPTH_TEST);
-	}
-	void Render() {
-		Reset();
-		//glEnable(GL_TEXTURE);
-		glUseProgram(shader.id);
-#ifdef VAO_SUPPORT
-		glBindVertexArray(vao);
-#else
-		// render target quad vertices
-		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, vbo1);
-		glVertexAttribPointer(shader.aPos,
-			3,
-			GL_FLOAT,
-			GL_FALSE,
-			0,
-			(void*)0);
-		// render target uv
-		glEnableVertexAttribArray(1);
-		glBindBuffer(GL_ARRAY_BUFFER, vbo2);
-		glVertexAttribPointer(shader.aRT,
-			2,
-			GL_FLOAT,
-			GL_FALSE,
-			0,
-			(void*)0);
-		// mask uv
-		glEnableVertexAttribArray(2);
-		glBindBuffer(GL_ARRAY_BUFFER, mask_uv);
-		glVertexAttribPointer(shader.aMask,
-			2,
-			GL_FLOAT,
-			GL_FALSE,
-			0,
-			(void*)0);
-#endif
-		glUniform1f(shader.uMaskOpacity, maskOpacity);
-		glUniform1f(shader.uMaskVRepeat, maskRepeat);
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, txt);
-		glUniform1i(shader.uSmpRT, 0);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, mask);
-		glUniform1i(shader.uSmpMask, 1);
-		glUniform2f(shader.uScreenSize, (GLfloat)globals.width, (GLfloat)globals.height);
-
-		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-		//glDisable(GL_TEXTURE);
-		glActiveTexture(GL_TEXTURE0);
-#ifndef VAO_SUPPORT
-		glDisableVertexAttribArray(0);
-		glDisableVertexAttribArray(1);
-		glDisableVertexAttribArray(2);
-#endif
-	}
-	~RT() {
-		glDeleteFramebuffers(1, &fbo);
-		glDeleteRenderbuffers(1, &rbo);
-		glDeleteBuffers(1, &vbo1);
-		glDeleteBuffers(1, &vbo2);
-		glDeleteBuffers(1, &mask_uv);
-#ifdef VAO_SUPPORT
-		glDeleteVertexArrays(1, &vao);
-#endif
 	}
 };
 
@@ -1069,7 +911,7 @@ struct Renderer {
 				std::sin(glm::two_pi<float>() / steps * i) * ratio << "f,.0f,\n";
 		}
 	}
-	Renderer(Asset::Assets& assets, const AABB& scene_bounds) : assets(assets) {
+	Renderer(Asset::Assets& assets, const AABB& scene_bounds) : assets(assets), rt(globals.width, globals.height) {
 		Init(scene_bounds);
 	}
 	void Init(const AABB& scene_bounds) {
